c4b2edf208b2a64ff157de922457fe7b2e2a853a
tony lian <tony.lian@chinaventure.com.cn>
2017-11-24 09:10:39 +0800

    添加包

---
+++ Tony/src/main/java/com/example/demo/TonyApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonyApplication.class, args);
+	}
+}
